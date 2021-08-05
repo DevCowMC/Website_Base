6d9f7bdd3fc4455a3293ad4b9616f78ed99a7a41
DevCowMC <kieranhouse@icloud.com>
2021-08-05 04:47:27 +0100

    Initial commit

---
+++ includes/links.php
@@ -0,0 +1,6 @@
+<?php session_start(); ?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="../assets/css/master.css">
+<link rel="stylesheet" href="../assets/css/navbar.css">
+<script src="https://kit.fontawesome.com/636a668978.js" crossorigin="anonymous"></script>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php require "./includes/links.php"; ?>
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<?php require "./includes/header.php"; ?>
+<?php require "./includes/navbar.php"; ?>
+
+<?php require "./includes/footer.php"; ?>
+<?php require "./includes/scripts.php"; ?>
+</body>
+</html>
